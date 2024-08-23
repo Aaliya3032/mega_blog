6da35c32418ca71ad545e69b2dbd12cdce5b7f9b
Aaliya3032 <khan.alia3032@gmail.com>
2024-08-23 20:48:26 +0530

    first commit mega blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux"
+import  authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer"
+import { Outlet } from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData)=>{
+     if(userData){
+        dispatch(login({userData}))
+        console.log("login");
+     }else{
+      dispatch(logout())
+      console.log("logout");
+     }
+  })
+  .finally(()=>{ setLoading(false)})
+},[]) 
+  
+  return !loading? (<div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="w-full block">
+      <Header/>
+      <main>
+       Todo: <Outlet/>
+      </main>
+      <Footer/>
     </div>
-  );
+  </div>) : null;
 }
 
 export default App;
---
+++ src/conf/conf.js
@@ -0,0 +1,17 @@
+const conf = {
+    appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+    appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID)
+}
+
+// const conf = {
+// appwriteUrl: process.env.REACT_APP_APPWRITE_URL,
+// appwriteProjectId: process.env.REACT_APP_APPWRITE_PROJECT_ID,
+// appwriteDatabaseId: process.env.REACT_APP_APPWRITE_DATABASE_ID,
+// appwriteCollectionId: process.env.REACT_APP_APPWRITE_COLLECTION_ID,
+// appwriteBucketId: process.env.REACT_APP_APPWRITE_BUCKET_ID
+// }
+
+export default conf;
