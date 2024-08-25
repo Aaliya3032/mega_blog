d8cce8e44871c99182449cfd2a53eb0f1d2dc617
Aaliya3032 <khan.alia3032@gmail.com>
2024-08-25 20:37:38 +0530

    dispatch dependencies

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ useEffect(()=>{
      }
   })
   .finally(()=>{ setLoading(false)})
-},[]) 
+},[dispatch]) 
   
   return !loading? (<div className="min-h-screen flex flex-wrap content-between bg-gray-400">
     <div className="w-full block">
